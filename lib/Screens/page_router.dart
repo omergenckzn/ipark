5da592bc98b20ab0294ab1bf921d03c80d506f41
Ahmet Can GÜNAY <1800002666@stu.iku.edu.tr>
2023-05-06 14:08:53 +0300

    test opencv

--- lib/Screens/page_router.dart
+++ lib/Screens/page_router.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:ipark/Screens/onBoarding/onBoardingWelcome/on_boarding_welcome_view.dart';
-import 'main_navigation_page.dart';
-
+import 'package:flutter/material.dart';
+import 'package:ipark/Screens/test_cpp.dart';
 
 class PageRouter extends StatefulWidget {
   const PageRouter({Key? key}) : super(key: key);
@@ -12,15 +10,11 @@ class PageRouter extends StatefulWidget {
 }
 
 class _PageRouterState extends State<PageRouter> {
-
   @override
   Widget build(BuildContext context) {
-
-
     bool isSignedIn = checkUserSignedIn();
-    return isSignedIn ? const MainNavigationPage() : const OnBoardingWelcomeView();
+    return isSignedIn ? const TestCpp() : const TestCpp();
   }
-
 
   bool checkUserSignedIn() {
     FirebaseAuth auth = FirebaseAuth.instance;
@@ -30,5 +24,4 @@ class _PageRouterState extends State<PageRouter> {
       return false;
     }
   }
-
 }
