3df2fc85d3fcb36f2479910386bc917f6171dcfb
omergencKzNN <omergenc.me@gmail.com>
2023-05-09 13:45:54 +0300

    testing

--- lib/Screens/page_router.dart
+++ lib/Screens/page_router.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:ipark/Screens/main_navigation_page.dart';
+import 'package:ipark/Screens/onBoarding/onBoardingWelcome/on_boarding_welcome_view.dart';
 import 'package:ipark/Screens/test_cpp.dart';
 
 class PageRouter extends StatefulWidget {
@@ -12,8 +14,9 @@ class PageRouter extends StatefulWidget {
 class _PageRouterState extends State<PageRouter> {
   @override
   Widget build(BuildContext context) {
+
     bool isSignedIn = checkUserSignedIn();
-    return isSignedIn ? const TestCpp() : const TestCpp();
+    return isSignedIn ? const MainNavigationPage() : const OnBoardingWelcomeView();
   }
 
   bool checkUserSignedIn() {
